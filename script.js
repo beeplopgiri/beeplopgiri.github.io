7545f5cf6252e8fcb06c7d3be3b3ba81c8ba6054
beeplopgiri <biplop.giri@acem.edu.np>
2026-06-05 11:00:00 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const colorBtn = document.querySelector('#colorButton')
+const hoverBtn = document.querySelector("#hoverButton")
+
+const hero = document.querySelector('.hero')
+
+colorBtn.addEventListener('click', function () {
+    // hero.style.backgroundColor = 'black';
+    hero.style.fontSize = "3.5rem";
+    console.log("color button clicked")
+})
+
+hoverBtn.addEventListener('mouseover', function (){
+    hoverBtn.style.backgroundColor = "green"
+    console.log("hovered over the button")
+})
+
+hoverBtn.addEventListener('mouseout', function (){
+    hoverBtn.style.backgroundColor = ""
+    console.log("mouse away from the button")
+})
+
+
+
+const countBtn = document.querySelector("#countButton")
+const clicksLabel = document.querySelector("#clicks")
+
+let noOfclicks = 0;
+
+countBtn.addEventListener('click' , function(){
+    noOfclicks++;
+    clicksLabel.textContent = "No of clicks :" + noOfclicks; 
+    // clicksLabel.textContent = `No of clicks : ${noOfclicks}`;
+
+
+})
